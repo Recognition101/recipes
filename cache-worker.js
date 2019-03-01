e42cf23fb7b747a717cd4f8c1de7bf8c4f3beb90
Mike Wilson <mike@Mikes-MacBook-Pro.local>
2019-02-28 23:50:07 -0800

    feat(grid) recipes use grid layout feat(worker) use service worker for caching feat(images) remove remote images feat(js) statically typed js

---
+++ cache-worker.js
@@ -0,0 +1,48 @@
+const cacheId = 'cache-worker-v1';
+const selfUnknown = /** @type {unknown} */(self);
+const serviceWorker = /** @type {ServiceWorkerGlobalScope} */(selfUnknown);
+
+const isLongLived = /\.(jpg|jpeg|png)$/i;
+
+/**
+ * Gets a response from the cache, fetching a response upon a cache miss.
+ * @param {Request} request the request to lookup in the cache
+ * @return {Promise<Response>} the response, either from the cache or network
+ */
+const cacheGet = async request => {
+    const cache = await caches.open(cacheId);
+    const cacheResponse = await cache.match(request);
+    if (cacheResponse) {
+        return cacheResponse;
+    } else {
+        const response = await fetch(request);
+        await cache.put(request, response.clone());
+        return response;
+    }
+};
+
+/**
+ * Fetches a fresh response from the internet and caches the result.
+ * @param {Request} request the request to fetch from the internet
+ * @return {Promise<void>} a promise that resolves when the result is cached
+ */
+const cacheRefresh = async request => {
+    if (!isLongLived.test(request.url)) {
+        const cache = await caches.open(cacheId);
+        const response = await fetch(request);
+        await cache.put(request, response);
+    }
+};
+
+serviceWorker.addEventListener('install', ev => {
+    ev.waitUntil(caches.open(cacheId).then(cache => cache.addAll([
+        './index.html',
+        './scripts.js',
+        './styles.css'
+    ])));
+});
+
+serviceWorker.addEventListener('fetch', ev => {
+    ev.respondWith(cacheGet(ev.request));
+    ev.waitUntil(cacheRefresh(ev.request));
+});
--- scripts.js
+++ scripts.js
@@ -1,36 +1,79 @@
-var firstParentMatch = function(el, sel) {
-    while(!!el && el.matches && !el.matches(sel)) { el = el.parentNode; }
-    return el;
+/**
+ * Finds elements within a given root that match a given selector
+ * @param {string} sel find all elements within `el` that match this selector
+ * @param {HTMLElement} [el] the root element to search within
+ * @return {HTMLElement[]} the elements that matched `sel` within `el`
+ */
+const $$ = (sel, el=document.body) => Array.from(el.querySelectorAll(sel));
+
+/**
+ * Given a cell that starts a row, append the `data-add` attribute value all
+ * cells in this row to a given array of numbers.
+ * @param {number[]} cellHeights `data-add` values will be appended to this
+ * @param {Element|null} cell the cell that starts a row
+ */
+const addHeightsForRow = (cellHeights, cell) => {
+    while(cell) {
+        cellHeights.push(parseInt(cell.getAttribute('data-add') || '0') || 0);
+        cell = cell.nextElementSibling;
+        cell = cell && cell.classList.contains('start') ? null : cell;
+    }
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    var recipes = document.getElementsByClassName('recipe');
-    for(var i=0; i < recipes.length; i+=1) {
-        recipes[i].classList.add('hidden');
-    }
-    document.addEventListener('click', function(ev) {
-        var toggle = ev.target.tagName !== 'A' && 
-                     firstParentMatch(ev.target, '.recipe-toggle');
-        if (toggle) {
-            var nextRecipe = toggle.nextElementSibling;
-            if (nextRecipe && nextRecipe.classList.contains('hidden')) {
-                nextRecipe.classList.remove('hidden');
-            } else if (nextRecipe) {
-                nextRecipe.classList.add('hidden');
-            }
-        }
+/**
+ * Gets the number of columns in a given grid.
+ * @param {HTMLElement} grid a grid to determine the column count of
+ * @return {number} the number of columns in this grid
+ */
+const getColumnCount = grid => {
+    let spanningCells = /** @type {number[]} */([]);
+    let maxLength = 0;
+
+    Array.from(grid.getElementsByClassName('start')).forEach(cell => {
+        addHeightsForRow(spanningCells, cell);
+        maxLength = Math.max(maxLength, spanningCells.length);
+        spanningCells = spanningCells.map(x => x - 1).filter(x => x > 0);
     });
+
+    return maxLength;
+};
+
+/**
+ * Adds the `c<width>` classes to each `.start` element in a grid.
+ * @param {HTMLElement} grid the grid whose row starts we will add classes to
+ */
+const addColumnSpans = grid => {
+    let spanningCells = /** @type {number[]} */([]);
+    const width = getColumnCount(grid);
+    grid.classList.add('grid-' + width);
+
+    Array.from(grid.getElementsByClassName('start')).forEach((cell, i) => {
+        addHeightsForRow(spanningCells, cell);
+        cell.classList.add('c' + (width - spanningCells.length + 1));
+        cell.classList.add(i % 2 === 0 ? 'start-even' : 'start-odd');
+        spanningCells = spanningCells.map(x => x - 1).filter(x => x > 0);
+    });
+};
+
+// ---- EVENT HANDLING AND SETUP ----
+
+document.addEventListener('click', ev => {
+    const target = /** @type {HTMLElement} */(ev.target);
+    const targetTag = target.tagName.toLowerCase();
+
+    const recipeBtn = target.closest('.recipe-toggle');
+    const recipeSwap = recipeBtn && recipeBtn.nextElementSibling;
+    const doSwap = recipeSwap && recipeSwap.classList.contains('recipe');
+
+    if (recipeBtn && recipeSwap && doSwap && targetTag !== 'a') {
+        const isVisible = recipeSwap.classList.contains('hidden');
+        recipeSwap.classList.toggle('hidden', !isVisible);
+    }
 });
 
-// Check if a new cache is available on page load.
-window.addEventListener('load', function(e) {
-    var ac = window.applicationCache;
-    if (ac) {
-        ac.addEventListener('updateready', function(e) {
-            if (ac.status === ac.UPDATEREADY) {
-                window.location.reload();
-            }
-        }, false);
-    }
+$$('.recipe').forEach(el => el.classList.add('hidden'));
+$$('.grid').forEach(el => addColumnSpans(el));
 
-}, false);
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./cache-worker.js', { scope: './' });
+}
